59e0e20b6e99a6761c5c08e31cda9ff5436b882c
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2022-08-08 15:24:02 +0900

    S3バケットを作成

--- main.ts
+++ main.ts
@@ -1,11 +1,19 @@
 import { Construct } from "constructs";
 import { App, TerraformStack } from "cdktf";
+import { AwsProvider } from "@cdktf/provider-aws";
+import { S3Bucket } from "@cdktf/provider-aws/lib/s3";
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, name: string) {
     super(scope, name);
 
-    // define resources here
+    new AwsProvider(this, 'aws', {
+      region: 'ap-northeast-1',
+    })
+
+    new S3Bucket(this, 'cdktf-test', {
+      bucket: 'cdktf-test-sakamoto',
+    })
   }
 }
 
